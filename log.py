d00168a87a42d7f5d619e6294318f1ddcd000751
Nlegion <nparanoid@gmail.com>
2022-08-01 10:23:46 +0300

    010822
    010822

--- log.py
+++ log.py
@@ -11,5 +11,5 @@ if __name__ == '__main__':
     stream = logging.StreamHandler()
     stream.setFormatter(settings)
     log.addHandler(stream)
-    # В логгирование передаем имя текущей функции и имя вызвавшей функции
+    # Test message
     log.info('Отладочное сообщение')
